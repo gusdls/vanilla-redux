33e7289b7c57d9efbd149de79f78fb3771065514
Dohyun Kim <0704dohyun@gmail.com>
2022-03-01 17:20:58 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const result = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, { type }) => {
+  if (type === "PLUS") {
+    return count + 1;
+  } else if (type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
 
+countStore.dispatch({ type: "PLUS" });
 console.log(countStore.getState());
